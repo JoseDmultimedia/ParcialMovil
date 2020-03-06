0ddc2e85e4a6351cd8fa84578fd32d5de71e38e3
Jonathan <jonathan.giron@uao.edu.co>
2020-03-06 16:24:07 -0500

    Este es el punto 3 sin terminar

---
+++ TercerPunto/app/src/main/java/com/example/tercerpunto/Gestionar.java
@@ -0,0 +1,14 @@
+package com.example.tercerpunto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gestionar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestionar);
+    }
+}
---
+++ TercerPunto/app/src/main/java/com/example/tercerpunto/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tercerpunto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bGestion, bCalcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Calcular(View view){
+        Intent i = new Intent(MainActivity.this,Calcular );
+
+
+    }
+}
